0e694acbbb06873e6aa7d13563397e9fe4820297
shansally <sln6124639@gmail.com>
2024-04-09 15:48:52 +0800

    git main init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+    
+
+
+
+
+
+
